2d39636342427a416d93b831e7df8e46862f062b
skechucky <danny_el_94@yahoo.com>
2018-04-27 23:56:48 +0300

    fixed

--- MPI_2_ADP_/Source.cpp
+++ MPI_2_ADP_/Source.cpp
@@ -7,36 +7,38 @@
 int main(int argc, char** argv) {
 
 	MPI_Status status;
-	int procsSize, idProc, i, oldRandom, oldRank,random;
-	bool lider = true;
-	srand(time(NULL));
-	random = rand();
+	int procsSize, idProc, i, oldRandom, oldIdProc, random, lider = 0;
 
 	MPI_Init(&argc, &argv);
 
 	MPI_Comm_rank(MPI_COMM_WORLD, &idProc);
 	MPI_Comm_size(MPI_COMM_WORLD, &procsSize);
 
-
+	srand(time(NULL) + idProc*idProc + idProc);
+	random = rand();
+	printf("%d - rank %d", random, idProc);
+	oldRandom = random;
+	oldIdProc = idProc;
 	for (i = 0; i < procsSize; i++) {
 		if (i != idProc) {
 			MPI_Send(&random, 1, MPI_INT, i, 1, MPI_COMM_WORLD);
 			MPI_Recv(&oldRandom, 1, MPI_INT, i, 1, MPI_COMM_WORLD, &status);
 			if (random < oldRandom) {
-				lider = false;
+				//printf("1");
+				lider = 0;
 			}
 			else if (random == oldRandom) {
 				MPI_Send(&idProc, 1, MPI_INT, i, 1, MPI_COMM_WORLD);
-				MPI_Recv(&oldRank, 1, MPI_INT, i, 1, MPI_COMM_WORLD, &status);
-				if (idProc < oldRank) {
-					lider = false;
+				MPI_Recv(&oldIdProc, 1, MPI_INT, i, 1, MPI_COMM_WORLD, &status);
+				if (idProc < oldIdProc) {
+					lider = 0;
+					//printf("2");
 				}
 			}
 		}
 	}
-	printf("%d - rank %d",random, idProc);
-	if (lider==true) {
-		printf(" <<<-- Acesta este procesul lider.");
+	if (lider == 1) {
+		printf(" <<<-- This is the leader.");
 	}
 	MPI_Finalize();
 }
